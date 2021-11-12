685304a5e926807b73f42d49b4cd20dfb11d3ef3
J. W. Smith <jwsmith2spam at gmail dot com>
2021-11-12 14:56:45 -0500

    Move modifiers and layer switch to oneshot keys
    This is a substantial change and I'm going to have to actively
    re-acclimate myself to it...
    
    I went cold turkey and removed lgui and lctrl toggles.  These will be
    the hardest for me.  I almost never use alt/meta... that change will be
    comparatively easy.
    
    (the branch name is a pun on a name of a videogame that I really ought
    to finish: https://en.wikipedia.org/wiki/OneShot)

--- keyboards/ergodox_ez/keymaps/jws85/keymap.c
+++ keyboards/ergodox_ez/keymaps/jws85/keymap.c
@@ -64,14 +64,14 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,       KC_1,     KC_2,    KC_3,            KC_4,            KC_5,      KC_RALT,              KC_RALT,         KC_6,      KC_7,           KC_8,             KC_9,      KC_0,           KC_ESCAPE,
-    KC_TAB,          KC_Q,     KC_W,    KC_E,            KC_R,            KC_T,      LALT_T(KC_EQUAL),     LALT_T(KC_MINUS),KC_Y,      KC_U,           KC_I,             KC_O,      KC_P,           KC_BSLASH,
-    LCTL_T(KC_GRAVE),KC_A,     KC_S,    KC_D,            KC_F,            KC_G,                                             KC_H,      KC_J,           KC_K,             KC_L,      KC_SCOLON,      RCTL_T(KC_QUOTE),
-    KC_LSHIFT,       KC_Z,     KC_X,    KC_C,            KC_V,            KC_B,      KC_LBRACKET,          KC_RBRACKET,     KC_N,      KC_M,           KC_COMMA,         KC_DOT,    KC_SLASH,       KC_RSHIFT,
-    MO(ADMIN),       _______,  _______, TD(DANCE_MOUSE0),TD(DANCE_FN0),                                                                TD(DANCE_FN1),  TD(DANCE_MOUSE1), MO(MEDIA), _______, MO(ADMIN),
-                                                                          _______,   _______,              _______,         _______,
-                                                                                     _______,              _______,
-                                                         LGUI_T(KC_SPACE),KC_BSPACE, _______,              _______,         KC_DELETE, LGUI_T(KC_ENTER)
+    KC_ESCAPE, KC_1,     KC_2,    KC_3,          KC_4,          KC_5,      KC_RALT,              KC_RALT,         KC_6,       KC_7,           KC_8,          KC_9,    KC_0,      KC_ESCAPE,
+    KC_TAB,    KC_Q,     KC_W,    KC_E,          KC_R,          KC_T,      LALT_T(KC_EQUAL),     LALT_T(KC_MINUS),KC_Y,       KC_U,           KC_I,          KC_O,    KC_P,      KC_BSLASH,
+    KC_GRAVE,  KC_A,     KC_S,    KC_D,          KC_F,          KC_G,                                             KC_H,       KC_J,           KC_K,          KC_L,    KC_SCOLON, KC_QUOTE,
+    KC_LSHIFT, KC_Z,     KC_X,    KC_C,          KC_V,          KC_B,      KC_LBRACKET,          KC_RBRACKET,     KC_N,       KC_M,           KC_COMMA,      KC_DOT,  KC_SLASH,  KC_RSHIFT,
+    MO(ADMIN), _______,  _______, OSM(MOD_LGUI), OSM(MOD_LCTL),                                                               OSM(MOD_LCTL),  OSM(MOD_LGUI), _______, _______,   MO(ADMIN),
+                                                                OSL(FN),   OSL(MOUSE),           _______,         OSL(MEDIA),
+                                                                           _______,              _______,
+                                                 KC_SPACE,      KC_BSPACE, _______,              _______,         KC_DELETE,  KC_ENTER
   ),
   [FN] = LAYOUT_ergodox_pretty(
     TO(BASE), KC_F1,   KC_F2,   KC_F3,   KC_F4,         KC_F5,          _______,         _______, KC_F6,       KC_F7,     KC_F8,   KC_F9,    KC_F10,  TO(BASE),
