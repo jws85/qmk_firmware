3adbcfe50aad16684a5897eb6560e2ffc2458627
J. W. Smith <jwsmith2spam at gmail dot com>
2021-11-18 21:41:31 -0500

    Configuration that may not murder my wrists
    Whew...
    
    Still not 100% on this, but it's already a lot better.
    
     - Just gave in and realized I need to do layer triggers for some
       things (though bucky bit mods maybe not)...
    
     - A tiny bit of inspiration hit... holding down func layer and
       mouse layer keys puts you in media keys layer
    
     - Furthermore got rid of admin layer.  I never used the REISUB
       capability (for all two linux machines that remember that) and
       that basically leaves behind LED brightness and keyboard reset.
       These are great things to put in the 1u thumb cluster keys that
       my hugely huge hands that are totally not the size of one gold
       sharpie obsessed tinpot tyrant, cannot hit ever.  Better yet,
       they're in the func layer.  So I'd have to hold func and push
       the button to reset, which is fine.
    
     - Bucky bit mods are OSMs so I can arpeggio the keys and not
       hold them down.  So shutting down Emacs (which hopefully I'll
       not be doing much longer :P) is just C, x, C, c.  No holding
       needed.  The tap toggle is 9 to prevent going into bucky bit
       lock which sucks.  It is much less important to be able to
       arpeggio the layer keys, since they are used less.
    
    Things that still need work:
    
     - Hitting F1-F5 and F11 currently would require contortion, holding
       down func and then hitting 1-5 or t.  Could be fixed by making
       Enter an LT as well.
    
     - Now that we've got LTs again, we're back to the usual problems.
       Since I shouldn't be chording too much with these keys, the
       biggest one is that holding it down no longer emits many spaces
       or backspaces.  Ah well.
    
     - Not sure about win/alt placement.  ctl is staying where it is
       though!

--- keyboards/ergodox_ez/keymaps/jws85/keymap.c
+++ keyboards/ergodox_ez/keymaps/jws85/keymap.c
@@ -2,84 +2,69 @@
 #include "version.h"
 #include "keymap_us_international.h"
 
+enum layer_names {
+    LAYER_BASE,
+    LAYER_FUNC,
+    LAYER_MOUSE,
+    LAYER_MEDIA
+};
+
+#define LED_LVL LED_LEVEL
+
 #define WEB_BCK LALT(KC_LEFT)
 #define WEB_FWD LALT(KC_RIGHT)
 #define WEB_RLD LALT(KC_R)
 #define WEB_NTB LALT(KC_T)
 #define WEB_CTB LALT(KC_W)
 
-#define OSL_FN OSL(FN)
-#define OSL_MOU OSL(MOUSE)
-#define OSL_MDA OSL(MEDIA)
+#define SPC_FUN LT(LAYER_FUNC, KC_SPC)
+#define BCK_MOU LT(LAYER_MOUSE, KC_BSPC)
+#define MO_MEDA MO(LAYER_MEDIA)
 
-#define CONTROL KC_LCTL
-#define META KC_LALT
-#define SUPER KC_LGUI
-#define ADMIN MO_ADMIN
-#define LED_LVL LED_LEVEL
-
-enum layer_names {
-    BASE,
-    FN,
-    MOUSE,
-    MEDIA,
-    ADMIN
-};
-
-enum custom_keycodes {
-  RGB_SLD = EZ_SAFE_RANGE,
-};
+#define MOD_CTL OSM(MOD_LCTL)
+#define MOD_ALT OSM(MOD_LALT)
+#define MOD_WIN OSM(MOD_LGUI)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_ergodox_pretty(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    META,       META,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    SUPER,      SUPER,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+  [LAYER_BASE] = LAYOUT_ergodox_pretty(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,    _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_ESC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,     KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
     KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    CONTROL,    CONTROL, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    ADMIN,   _______, _______, KC_EQL,  KC_MINS,                                        KC_LBRC, KC_RBRC, _______, _______, ADMIN,
-                                                 OSL_FN,  OSL_MOU,    _______, OSL_MDA,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MOD_WIN,    MOD_WIN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    _______, _______, _______, MOD_ALT, MOD_CTL,                                        MOD_CTL, MOD_ALT, KC_LBRC, KC_RBRC, _______,
+                                                 _______, _______,    _______, _______,
                                                           _______,    _______,
-                                        KC_SPC,  KC_BSPC, _______,    _______, KC_DEL,  KC_ENTER
+                                        SPC_FUN, BCK_MOU, _______,    _______, KC_DEL,  KC_ENTER
   ),
-  [FN] = LAYOUT_ergodox_pretty(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
+  [LAYER_FUNC] = LAYOUT_ergodox_pretty(
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_ESC,
     _______, _______, _______, _______, _______, KC_F11,  KC_F11,     KC_F12,  KC_F12,  _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
     _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
-                                                 _______, _______,    _______, _______,
+                                                 _______, _______,    RESET,   LED_LVL,
                                                           _______,    _______,
-                                        _______, _______, _______,    _______, KC_CAPS, KC_PSCREEN
+                                        _______, MO_MEDA, _______,    _______, KC_CAPS, KC_PSCREEN
   ),
-  [MOUSE] = LAYOUT_ergodox_pretty(
-    _______, _______, _______, _______, _______, _______, _______,    KC_ACL0, _______, _______, _______, _______, _______, _______,
+  [LAYER_MOUSE] = LAYOUT_ergodox_pretty(
+    KC_ESC,  _______, _______, _______, _______, _______, _______,    KC_ACL0, _______, _______, _______, _______, _______, KC_ESC,
     _______, _______, _______, _______, _______, _______, _______,    KC_ACL1, WEB_CTB, WEB_BCK, WEB_FWD, WEB_NTB, _______, _______,
     _______, _______, _______, _______, _______, _______,                      KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,    KC_ACL2, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
     _______, _______, _______, _______, _______,                                        KC_BTN3, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                                           _______,    _______,
-                                        _______, _______, _______,    _______, KC_BTN2, KC_BTN1
+                                        MO_MEDA, _______, _______,    _______, KC_BTN2, KC_BTN1
   ),
-  [MEDIA] = LAYOUT_ergodox_pretty(
+  [LAYER_MEDIA] = LAYOUT_ergodox_pretty(
+    KC_ESC,  _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, KC_ESC,
     _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_VOLU, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_MPRV, KC_VOLD, KC_MNXT, _______, _______,                      _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, KC_MUTE,    _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,                                        _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,                      KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,                                        KC_MUTE, _______, _______, _______, _______,
                                                  _______, _______,    _______, _______,
                                                           _______,    _______,
-                                        KC_MPLY, KC_MSTP, _______,    _______, _______, _______
-  ),
-  [ADMIN] = LAYOUT_ergodox_pretty(
-    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, LALT(KC_PSCREEN),
-    KC_R,    KC_E,    KC_I,    KC_S,    KC_U,    KC_B,                          _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,                                         _______, _______, _______, _______, _______,
-                                                 _______, RESET,       RESET,   _______,
-                                                          _______,     _______,
-                                        LED_LVL, _______, _______,     _______, _______, LED_LVL
+                                        _______, _______, _______,    _______, KC_MSTP, KC_MPLY
   ),
 };
 
